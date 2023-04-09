837d8d767c92d0192e7b4b2bfe146410e040e492
daemon <0xDAEF0F@proton.me>
2023-04-08 21:28:37 -0600

    08_04_2023

--- largest-prime-factor.hs
+++ largest-prime-factor.hs
@@ -1,12 +1,25 @@
 import Data.List (find)
 
--- Largest prime factor --
+-- CHALLENGE
+-- Find the largest prime factor of n.
+
 -- The prime factors of 13195 are 5, 7, 13 and 29.
 -- What is the largest prime factor of the number 600_851_475_143 ?
 
-largestPrimeFactorForN :: Integral a => a -> Maybe a
-largestPrimeFactorForN n = find (\prime -> n `mod` prime == 0) (sieveOfEratosthenes [] [2 .. n])
+largestPrimeFactor :: Integral a => a -> Maybe a
+largestPrimeFactor n = find (\prime -> n `mod` prime == 0) (reverse (primeFactors n))
 
-sieveOfEratosthenes :: Integral a => [a] -> [a] -> [a]
-sieveOfEratosthenes primes [] = primes
-sieveOfEratosthenes primes (x : xs) = sieveOfEratosthenes (x : primes) (filter (\num -> (num `mod` x) /= 0) xs)
+primeFactors :: Integral a => a -> [a]
+primeFactors n = go n 2
+  where
+    go num divisor
+      | num == 1 = []
+      | num `mod` divisor == 0 = divisor : go (num `div` divisor) divisor
+      | otherwise = go num (divisor + 1)
+
+-- Generates all prime numbers up to N using sieveOfEratosthenes
+allPrimesUpToN :: Integral a => a -> [a]
+allPrimesUpToN n = sieveOfEratosthenes [] [2 .. n]
+  where
+    sieveOfEratosthenes primes [] = reverse primes
+    sieveOfEratosthenes primes (x : xs) = sieveOfEratosthenes (x : primes) (filter (\num -> (num `mod` x) /= 0) xs)
