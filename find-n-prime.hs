f49e93ae93572ab8c41158bc6f49f9f95ad78ab4
daemon <0xDAEF0F@proton.me>
2023-04-09 10:55:41 -0600

    09_04_2023

---
+++ find-n-prime.hs
@@ -0,0 +1,11 @@
+-- CHALLENGE
+-- By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+-- What is the 10 001st prime number?
+
+tenThousandAndOnePrimeNumber :: Integer
+tenThousandAndOnePrimeNumber = last $ take 10_001 allPrimeNumbers
+
+allPrimeNumbers :: [Integer]
+allPrimeNumbers = sieve [2 ..]
+  where
+    sieve (x : xs) = x : sieve (filter (\num -> (num `mod` x) /= 0) xs)
