d467b49e1f767e3444e26cb8b241fb64f2ea3623
daemon <0xDAEF0F@proton.me>
2023-04-08 19:10:43 -0600

    08_04_2023

--- largest-prime-factor.hs
+++ largest-prime-factor.hs
@@ -1,9 +1,11 @@
+import Data.List (find)
+
 -- Largest prime factor --
 -- The prime factors of 13195 are 5, 7, 13 and 29.
 -- What is the largest prime factor of the number 600_851_475_143 ?
 
-largestPrimeFactorAnswer :: Integer
-largestPrimeFactorAnswer = head $ sieveOfEratosthenes [] [2 .. 600_851_475_142]
+largestPrimeFactorForN :: Integral a => a -> Maybe a
+largestPrimeFactorForN n = find (\prime -> n `mod` prime == 0) (sieveOfEratosthenes [] [2 .. n])
 
 sieveOfEratosthenes :: Integral a => [a] -> [a] -> [a]
 sieveOfEratosthenes primes [] = primes
