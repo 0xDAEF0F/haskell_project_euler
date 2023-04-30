2e74cacfe64334afc5df71b67b37859f2ed3c784
daemon <0xDAEF0F@proton.me>
2023-04-29 18:14:12 -0600

    29_04_2023

---
+++ 29_04_23_amicable_numbers.hs
@@ -0,0 +1,16 @@
+sumAllAmicableNumbersUnderTenThousand :: Integer
+sumAllAmicableNumbersUnderTenThousand = sum $ filter isAmicableNumber [2 .. 9_999]
+
+isAmicableNumber :: Integral a => a -> Bool
+isAmicableNumber n = childSum == n && sumOfDivisors /= childSum
+  where
+    sumOfDivisors = sum $ divisorsOfNumber n
+    childSum = sum $ divisorsOfNumber sumOfDivisors
+
+divisorsOfNumber :: Integral t => t -> [t]
+divisorsOfNumber = go 1
+  where
+    go n t
+      | n == t = []
+      | t `mod` n == 0 = n : go (n + 1) t
+      | otherwise = go (n + 1) t
